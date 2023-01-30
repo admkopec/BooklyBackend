74b3035a28de1f835b1fb90424bbb8de2c3420a5
Adam Kopeć <adamkopec5@icloud.com>
2023-01-30 17:44:05 +0100

    Quick fix for Carly (but still doesn't work)

--- src/main/java/pw/react/tuesday_booklybackend/web/ReservationCarlyDto.java
+++ src/main/java/pw/react/tuesday_booklybackend/web/ReservationCarlyDto.java
@@ -9,7 +9,7 @@ import javax.validation.constraints.FutureOrPresent;
 import javax.validation.constraints.NotEmpty;
 import java.util.UUID;
 
-public record ReservationCarlyDto(UUID id, @NotEmpty String name, @FutureOrPresent long startDate, @Future long endDate, @NotEmpty UUID carId, @NotEmpty String lastname) {
+public record ReservationCarlyDto(UUID id, String userId, @NotEmpty String name, @FutureOrPresent long startDate, @Future long endDate, @NotEmpty UUID carId, @NotEmpty String lastname) {
     public static Reservation convertToReservation(ReservationCarlyDto reservationDto, CompanionService service) {
         ReservationModificationDto reservation = new ReservationModificationDto(reservationDto.id(), reservationDto.name(), reservationDto.startDate(), reservationDto.endDate(), reservationDto.carId(), null, null, null);
         return ReservationModificationDto.convertToReservation(reservation, service);
@@ -26,7 +26,7 @@ public record ReservationCarlyDto(UUID id, @NotEmpty String name, @FutureOrPrese
             firstName = names[0];
             lastName = "";
         }
-        ReservationCarlyDto reservation = new ReservationCarlyDto(reservationDto.id(), firstName, reservationDto.dateFrom(), reservationDto.dateTo(), reservationDto.offerId(), lastName);
+        ReservationCarlyDto reservation = new ReservationCarlyDto(reservationDto.id(), "80253CBB-455D-4C6D-ADF9-BF6B8B4ABEF7", firstName, reservationDto.dateFrom(), reservationDto.dateTo(), reservationDto.offerId(), lastName);
         return reservation;
     }
 }
